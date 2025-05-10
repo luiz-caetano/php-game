5997ef2f52f4af708cda211bd3f5a3e1ecb5a4ec
Luiz Caetano <luizeduardosilvacaetano@gmail.com>
2025-05-10 14:03:36 -0300

    classe monstro criada

--- src/Model/Arma.php
+++ src/Model/Arma.php
@@ -27,5 +27,4 @@ class Arma
         return $this->tipo;
     }
 
-
 }
---
+++ src/Model/Monstro.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+class Monstro
+{
+    private string $nome;
+    private string $pv;
+    private string $tipo;
+}
