13df01c5f2274ac63d8390a46eccbbd73bd4cd1b
Luiz Caetano <luizeduardosilvacaetano@gmail.com>
2025-05-10 14:59:43 -0300

    classe monstro criada

--- src/Model/Monstro.php
+++ src/Model/Monstro.php
@@ -5,6 +5,27 @@ namespace App\Model;
 class Monstro
 {
     private string $nome;
-    private string $pv;
+    private int $pv;
     private string $tipo;
+
+    public function __construct(string $nome, int $pv, string $tipo)
+    {
+        $this->nome = $nome;
+        $this->pv = $pv;
+        $this->tipo = tipo;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function getPv(): int {
+        return $this->pv;
+    }
+
+    public function getTipo(): string {
+        return $this->tipo;
+    }
+
+
 }
